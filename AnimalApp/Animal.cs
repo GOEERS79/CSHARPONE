22801b564ed9b30b45efc72fe76f5e43f6fb18c5
Joe <jintorre@gmail.com>
2018-01-13 11:06:05 -0500

    Example change, add property color

---
+++ AnimalApp/Animal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalApp
+{
+    abstract class Animal
+    {
+        private int _weight;
+        //property
+        public int Weight
+        {
+            get { return _weight; }
+            set { _weight = value; }
+        }
+        public abstract void Eat();
+        public void MakeSound()
+        {
+            Console.WriteLine("Animal makesound ...");
+        }
+        public Animal()
+        {
+            Console.WriteLine("Animal base ...");
+        }
+        public virtual void myMethod()
+        {
+            Console.WriteLine("Animal mymethod ...");
+        }
+    }
+}
